a2f11d5f274b5a19d01e84e5ec1565c8c8bd4e12
Arsh Kumar Singh <arshk4219@gmail.com>
2024-06-08 16:31:24 +0530

    gui error handling(basic)

--- spacersrc/gui.py
+++ spacersrc/gui.py
@@ -1,20 +1,28 @@
 # spacersrc/gui.py
 
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from spacersrc.analyzer import scan_directory
 from spacersrc.visualizer import visualize_data
 
 def select_directory():
-    path = filedialog.askdirectory()
-    if path:
+    try:
+        path = filedialog.askdirectory()
+        if not path:
+            raise ValueError("No directory selected.")
         data = scan_directory(path)
+        if not data:
+            raise ValueError("No data to visualize.")
         visualize_data(data, 
                        title='Disk Usage Visualization', 
                        max_label_length=30, 
                        figsize=(12, 8), 
                        cmap='viridis', 
                        base_fontsize=10)
+    except ValueError as ve:
+        messagebox.showerror("Error", f"ValueError: {ve}")
+    except Exception as e:
+        messagebox.showerror("Error", f"An unexpected error occurred: {e}")
 
 def create_gui():
     root = tk.Tk()
